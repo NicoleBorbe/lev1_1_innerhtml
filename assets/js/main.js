006b1d9d9f1cbea38050d633b399c7814b7d4364
NicoleBorbe <nicole.borbe@gmail.com>
2022-08-18 13:13:04 +0200

    version1

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+document.write("<h1>Hello World</h1>");
+document.write("<p>Have a nice day!</p>");
+let myText = "Hello again <br>";
+document.write(myText);
+let vorname = "<br>Name: Nicole<br>";
+document.write(vorname);
+let nachname = "Nachname: Borbe";
+document.write(nachname);
